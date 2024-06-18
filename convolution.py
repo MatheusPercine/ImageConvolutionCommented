961aa8e98bcaa7204d333c36021ce5509d3bee15
MatheusPercine <matheuspercine@gmail.com>
2024-06-18 19:51:30 -0300

    Commit inicial

--- convolution.py
+++ convolution.py
@@ -60,6 +60,32 @@ def calculate_convolution_padless(image: np.ndarray, x, y, c, kernel: np.ndarray
     :return: The convoluted pixel
     """
 
+    """
+    A função calculate_convolution_padless no arquivo convolution.py é responsável por calcular a convolução de um pixel específico em uma imagem, aplicando um kernel sem adicionar padding (zeros ao redor da imagem). Isso significa que a convolução é calculada apenas para os pixels que têm todos os seus vizinhos necessários dentro dos limites da imagem.
+
+    A importância do "padless" se dá porque isso evita distorções causadas pelo padding, mas também significa que a convolução não é calculada para pixels nas bordas da imagem onde o kernel se estenderia além dos limites da imagem.
+
+    A função começa determinando as dimensões do kernel (kernel_height e kernel_width). Depois, calcula o centro do kernel (kernel_center), que é usado para alinhar o kernel corretamente com o pixel atual. As dimensões da imagem (image_height, image_width, _) são obtidas para verificar os limites da imagem durante a convolução.
+
+    'result' é inicializado como 0. Este valor acumulará a soma ponderada dos valores dos pixels vizinhos multiplicados pelos valores correspondentes do kernel.
+
+    Em seguida, começa a iteração sobre o kernel da seguinte forma:
+
+    Dois loops aninhados percorrem cada elemento do kernel.
+    Para cada elemento (i, j) do kernel, a posição correspondente na imagem (x_k, y_k) é calculada:
+    x_k = x - kernel_center[0] + i
+    y_k = y - kernel_center[1] + j
+    Isso desloca o kernel ao redor do pixel atual (x, y).
+
+    Os limites da Imagem são verificados.
+    Se a posição (x_k, y_k) estiver fora dos limites da imagem (menor que 0 ou maior que as dimensões da imagem), o loop continua sem adicionar ao resultado (continue).
+
+    Porém, caso a posição (x_k, y_k) estiver dentro dos limites da imagem, o valor do pixel correspondente na imagem image[x_k, y_k, c] é multiplicado pelo valor do kernel kernel[i, j] e somado ao result e a convolução no pixel é calculada.
+
+    Se new_image for fornecida, o resultado é armazenado na posição (x, y, c) da nova imagem.
+    A função retorna o resultado da convolução para o pixel atual.
+    """
+
     kernel_height, kernel_width = kernel.shape
     kernel_center = (kernel_height // 2, kernel_width // 2)
     image_height, image_width, _ = image.shape
@@ -88,6 +114,16 @@ def convolution(image: np.ndarray, kernel: np.ndarray) -> np.ndarray:
     :param kernel: The kernel to perform the convolution
     :return: The convoluted image
     """
+
+    """
+    Função que performa uma operação de convolução em uma imagem
+    Parametros: dados da imagem e kernel selecionado
+
+    Primeiramente, armazena-se, em variáveis, informações relativas à forma da image, como altura, largura e número de canais.
+    Depois, é criada uma matriz do mesmo formato da imagem original, porém preenchida com zeros. Esta matriz zerada se transformará na imagem convoluída.
+    Em seguida, é realizada a operação de convolução para cada pixel da imagem original, isto é, um pixel para cada canal, para cada altura, para cada largura da imagem original.
+    """
+
     image_height, image_width, image_channels = image.shape
 
     # Initialize the new image
@@ -110,6 +146,31 @@ def convolution_pool(image: np.ndarray, kernel: np.ndarray, num_processes: int) 
     :param num_processes: The number of processes to use
     :return: The convoluted image
     """
+
+    """
+    A função convolution_pool performa uma operação de convolução na imagem utilizando multiplos processos e para isso considera a estratégia de usar a classe Pool.
+
+    Cada processo pode tratar qualquer pixel da imagem. A tarefa de convolução de cada pixel é distribuída entre os processos disponíveis. Isso é feito através do método starmap do multiprocessing.Pool, que distribui a lista de tarefas (cada tarefa é calcular a convolução para um pixel específico) entre os processos.
+
+    A abordagem distribui a carga de trabalho de forma equilibrada, pois cada processo recebe uma tarefa (pixel) por vez, e conforme vai concluindo, pega a próxima tarefa disponível até que todas as tarefas sejam concluídas.
+    A funçaõ considera o balanceamento dinâmico: A distribuição dinâmica de tarefas ajuda a balancear a carga entre os processos. Nenhum processo fica ocioso enquanto houver tarefas pendentes.
+
+    Primeiramente, as dimensões da imagem (image_height, image_width, image_channels) são obtidas e 'new_image' é inicializada como uma matriz de zeros com as mesmas dimensões da imagem original.
+
+    A função cria um pool de processos usando o contexto gerenciador (with mp.Pool(num_processes) as pool), garantindo que os recursos sejam liberados automaticamente ao final da operação.
+
+    Em seguida, Para cada canal de cor (c) da imagem, pool.starmap é usada para mapear a função calculate_convolution_padless para cada pixel da imagem em paralelo, starmap permite passar múltiplos argumentos para a função, no caso (image, x, y, c, kernel). 
+    
+    Uma lista de tarefas é criada, onde cada tarefa é uma tupla de argumentos a ser passada para a função calculate_convolution_padless. Cada tupla representa a convolução a ser realizada em um pixel específico (x, y) de um canal específico c. 
+
+    starmap distribui essas tuplas de argumentos entre os processos do pool. Cada processo pega uma dessas tarefas e executa calculate_convolution_padless com os argumentos fornecidos.
+
+    O resultado é uma lista achatada de valores convolucionados, que é então remodelada para corresponder às dimensões originais da imagem (reshape(image_height, image_width)).
+
+    Os valores resultantes da convolução para cada canal são armazenados na new_image.
+
+    """
+
     image_height, image_width, image_channels = image.shape
 
     # Initialize the new image
@@ -136,6 +197,22 @@ def calculate_convolution_block(image: np.ndarray, x_i, y_i, x_f, y_f, c, kernel
     :param c: The channel of the pixel
     :param kernel: The kernel to perform the convolution
     :return: The convoluted block
+    """
+
+    """
+    A função calculate_convolution_block realiza a operação de convolução em um bloco específico de uma imagem e retorna o resultado desse bloco.
+
+    x_i, y_i são as coordenadas iniciais (superior esquerda) do bloco da imagem.
+    x_f, y_f são as coordenadas finais (inferior direita) do bloco da imagem.
+
+    A função começa determinando as dimensões do kernel (kernel_height e kernel_width). Depois, calcula o centro do kernel (kernel_center), que é usado para alinhar o kernel corretamente com o pixel atual. As dimensões da imagem (image_height, image_width, _) são obtidas para verificar os limites da imagem durante a convolução.
+
+    Um array de zeros é criado para armazenar o resultado da convolução do bloco especificado.
+
+    Na interação sobre o kernel, dois loops aninhados iteram sobre cada pixel do bloco especificado, dois loops aninhados iteram sobre cada elemento do kernel; calcula-se a posição do pixel da imagem a ser multiplicado pelo elemento correspondente do kernel; verifica-se se as coordenadas calculadas estão dentro dos limites da imagem. Se não estiverem, o loop continua para a próxima iteração, ignorando os valores fora dos limites; O valor do pixel da imagem é multiplicado pelo valor correspondente do kernel e somado ao resultado. 
+    
+    O bloco convolucionado é retornado como um array NumPy.
+
     """
 
     kernel_height, kernel_width = kernel.shape
@@ -167,6 +244,22 @@ def convolution_block(image: np.ndarray, kernel: np.ndarray, num_processes: int)
     :param num_processes: The number of processes to use
     :return: The convoluted image
     """
+
+    """
+    Nessa função, a operação de convolução segue a estratégia de dividir a imagem em blocos que são distribuídos entre diferentes processos. Cada processo trata de uma região específica (bloco) da imagem, calculando a convolução para todos os pixels dentro desse bloco. Após o processamento, os blocos são reunidos para formar a imagem convoluída final.
+
+    Primeiramente, informações sobre a imagem são armazenadas. Uma variável chamada result é inicializada e funcionará para armazenar o resultado de convoluções. A variável new_image é criada e que é uma nova matriz com a mesma forma (dimensões) da imagem original, onde cada elemento é do tipo np.uint8 (inteiro sem sinal de 8 bits). A matriz é criada sem valores iniciais definidos, o que é eficiente em termos de tempo, já que os valores iniciais serão sobrescritos durante o processamento da convolução.
+
+    A imagem é dividida em blocos horizontais, cada um com uma altura de chunk_size. Se o número de linhas não for divisível pelo número de processos, o último bloco inclui todas as linhas restantes.
+
+    Um pool de processos é criado com o número especificado de processos (num_processes).
+    Uma lista de tarefas é criada, onde cada tarefa é uma tupla de argumentos a ser passada para a função calculate_convolution_block. Cada tupla representa a convolução a ser realizada em um bloco específico da imagem (definido por x_i e x_f).
+
+    Para cada canal da imagem, starmap distribui essas tuplas de argumentos entre os processos do pool. Cada processo pega uma dessas tarefas e executa calculate_convolution_block com os argumentos fornecidos.
+
+    Os resultados dos blocos convoluídos são concatenados verticalmente (vstack) para reconstruir a imagem final para cada canal.
+    """
+
     image_height, image_width, image_channels = image.shape
 
     result = []
@@ -202,6 +295,19 @@ def convolution_numba(image: np.ndarray, kernel: np.ndarray, num_processes: int)
     :param num_processes: The number of processes to use
     :return: The convoluted image
     """
+
+    """
+    Função implementada utilizando a biblioteca Numba, que permite acelerar o código Python ao compilá-lo para código máquina usando just-in-time (JIT). 
+
+    A função é decorada com @jit(nopython=True, parallel=True). Isso instrui o Numba a compilar a função em modo nopython, que é a configuração mais rápida, e a paralelizar os loops onde possível.
+
+    Primeiramente, define-se o número de threads que o Numba deve utilizar para paralelizar a execução, obtém as dimensões da imagem original e inicializa um array de zeros com o mesmo formato para armazenar a imagem convolucionada.
+
+    Na operação de convolução, temos que, nesta ordem, para cada canal da imagem, utiliza-se prange (parallel range) para indicar que os loops devem ser paralelizados. Isso significa que cada iteração desses loops pode ser executada em paralelo, o que acelera significativamente a computação em máquinas com múltiplos núcleos; loops aninhados iteram sobre cada elemento do kernel; calcula-se a posição do pixel da imagem a ser multiplicado pelo elemento correspondente do kernel; verifica se as coordenadas calculadas estão dentro dos limites da imagem. Se não estiverem, a iteração continua, ignorando os valores fora dos limites; o valor do pixel da imagem é multiplicado pelo valor correspondente do kernel e somado ao resultado na nova imagem.
+
+    Finalmente, a função retorna a nova imagem convolucionada.
+    """
+
     set_num_threads(num_processes)
     image_height, image_width, image_channels = image.shape
 
--- main.py
+++ main.py
@@ -28,12 +28,48 @@ def main(argc, argv):
         case 1:
             selected_kernel = kernel.sharpen_kernel
         case 2:
+            """
+            TAMANHO DO KERNEL
+
+            Kernels maiores tendem a capturar mais detalhes e características das bordas na imagem, mas também podem suavizar pequenas variações. Isso ocorre porque um kernel maior abrange uma área maior da imagem, fazendo com que a média das diferenças de intensidade de pixel se espalhe por uma região maior. Portanto, um kernel maior pode detectar bordas mais gerais e menos sensíveis a pequenos detalhes. 
+            Kernels maiores aumentam o tempo de processamento, pois cada ponto da imagem precisa ser convolucionado com uma matriz maior. Em aplicações de tempo real ou com imagens de alta resolução, isso pode ser uma consideração importante.
+            Kernels maiores podem resultar em bordas mais espessas na imagem resultante, enquanto kernels menores tendem a produzir bordas mais finas. Isso ocorre porque a soma dos valores do kernel em um kernel maior se espalha por mais pixels, resultando em um efeito de "mancha" mais amplo.
+
+            Kernels menores, por outro lado, são mais sensíveis a pequenas variações nos níveis de intensidade dos pixels. Eles podem detectar bordas finas e detalhes pequenos com mais precisão. No entanto, isso pode resultar em uma imagem mais "ruidosa", com mais bordas detectadas em áreas onde há pequenas variações de intensidade que não são necessariamente bordas significativas.
+            """
             kernel_size = int(input("Enter the size of the kernel: "))
             selected_kernel = kernel.Kernel2D(kernel.edge_detection_kernel(kernel_size))
             # print(selected_kernel.kernel)
         case 3:
             selected_kernel = kernel.emboss_kernel
         case 4:
+            """
+            TAMANHO DO KERNEL
+
+            Kernels Gaussianos maiores produzem uma suavização mais forte na imagem. Isso ocorre porque um kernel maior abrange uma área maior da imagem e, consequentemente, a média ponderada dos valores dos pixels é calculada sobre uma área maior. Isso resulta na suavização de detalhes finos e na redução de ruídos na imagem.
+            Kernels Gaussianos menores proporcionam uma suavização mais leve. Eles afetam menos pixels ao redor de cada ponto, preservando mais detalhes e texturas na imagem. Isso pode ser útil quando você deseja suavizar ligeiramente uma imagem sem perder muita informação detalhada.
+
+            Kernels maiores aumentam o tempo de processamento, pois cada ponto da imagem precisa ser convolucionado com uma matriz maior. Em aplicações de tempo real ou com imagens de alta resolução, isso pode ser uma consideração importante.
+
+            Kernels Gaussianos maiores espalham o desfoque por uma área maior, resultando em uma aparência mais suave e borrada. Isso é útil para reduzir ruídos ou criar efeitos de desfoque artístico.
+            """
+
+            """
+            DESVIO PADRÃO
+
+            -Suavização:
+            Desvio padrão pequeno: Um sigma menor resulta em uma curva Gaussiana mais estreita, o que significa que os valores do kernel caem rapidamente à medida que se afastam do centro. Isso leva a uma suavização leve, onde apenas os pixels próximos ao ponto central contribuem significativamente para o valor do pixel convoluído. Detalhes finos são preservados.
+            Desvio padrão grande: Um sigma maior resulta em uma curva Gaussiana mais larga, o que significa que os valores do kernel caem mais lentamente. Isso leva a uma suavização mais forte, onde pixels mais distantes do ponto central também contribuem significativamente para o valor do pixel convoluído. Detalhes finos são suavizados ou borrados.
+
+            -Efeito de Desfoque:
+            Desvio padrão pequeno: O efeito de desfoque é menos pronunciado. A imagem resultante terá um desfoque leve, mantendo a maioria dos detalhes e texturas originais.
+            Desvio padrão grande: O efeito de desfoque é mais pronunciado. A imagem resultante terá um desfoque mais forte, com a redução de detalhes finos e a suavização de texturas.
+
+            -Amplitude da Influência:
+            Desvio padrão pequeno: A influência de cada pixel no resultado da convolução é restrita a uma área menor. Os pixels fora dessa área têm pouca ou nenhuma influência.
+            Desvio padrão grande: A influência de cada pixel no resultado da convolução se estende por uma área maior. Isso significa que mais pixels contribuem para o valor de cada pixel convoluído, resultando em uma mistura mais ampla dos valores dos pixels.
+            """
+
             kernel_size = int(input("Enter the size of the kernel: "))
             sigma = float(input("Enter the standard deviation of the Gaussian distribution: "))
             selected_kernel = kernel.Kernel2D(kernel.gaussian_kernel(kernel_size, sigma))
@@ -51,6 +87,24 @@ def main(argc, argv):
     model_choice = int(input("Enter the model number: "))
 
     if model_choice == 1:
+        """
+        1. Sequential
+
+        Forma sequencial de fazer a convolução
+
+        No arquivo convolution.py, temos a seguinte ordem de chamadas de função: 
+        convolution() --> calculate_convolution_padless()
+
+        Desvantagens do Modelo Sequencial:
+        Desempenho:
+        O processamento sequencial é lento para imagens grandes e kernels complexos devido à falta de paralelismo.
+        Cada pixel é processado individualmente, o que pode levar a um tempo de execução prolongado.
+
+        Vantagens do Modelo Sequencial:
+        Simplicidade:
+        A implementação é direta e fácil de entender.
+        Não há necessidade de lidar com problemas de sincronização ou comunicação entre threads/processos.
+        """
         start_time = time.time()
         print("Applying the kernel...")
         result_image = convolution.convolution(image.data, selected_kernel.kernel)
@@ -64,14 +118,32 @@ def main(argc, argv):
         print("Applying the kernel...")
         match model_choice:
             case 2:
+                """
+                2. Pool
+
+                No arquivo convolution.py, temos a seguinte ordem de chamadas de função: 
+                convolution_pool() --> calculate_convolution_padless()
+                """
                 result_image = convolution.convolution_pool(image.data, selected_kernel.kernel, num_threads)
                 end_time = time.time()
                 result_image = Image.from_data(result_image)
             case 3:
+                """
+                3. Block
+
+                No arquivo convolution.py, temos a seguinte ordem de chamadas de função: 
+                convolution_block() --> calculate_convolution_block()
+                """
                 result_image = convolution.convolution_block(image.data, selected_kernel.kernel, num_threads)
                 end_time = time.time()
                 result_image = Image.from_data(result_image)
             case 4:
+                """
+                4. Numba
+                No arquivo convolution.py, temos a seguinte ordem de chamadas de função: 
+                convolution_numba()
+                """
+
                 result_image = convolution.convolution_numba(image.data, selected_kernel.kernel, num_threads)
                 end_time = time.time()
                 result_image = Image.from_data(result_image)
